d36683258d1a33c70e36bec73b3a49ce12a56583
Klee Collage <klee.collage@gmail.com>
2024-10-10 15:25:55 -0500

    Add | Delete Tasks implemented

--- app/src/main/java/com/kleecollage/todoapp/addtask/ui/TaskViewModel.kt
+++ app/src/main/java/com/kleecollage/todoapp/addtask/ui/TaskViewModel.kt
@@ -1,8 +1,52 @@
 package com.kleecollage.todoapp.addtask.ui
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kleecollage.todoapp.addtask.ui.model.TaskModel
 import javax.inject.Inject
 
 class TaskViewModel @Inject constructor(): ViewModel() {
-    
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog:LiveData<Boolean> = _showDialog
+
+    private val _tasks = mutableStateListOf<TaskModel>()
+    val task: List<TaskModel> = _tasks
+
+    fun onShowDialog(show: Boolean) {
+        _showDialog.value = show
+    }
+
+    fun onTasksCreated(task: String){
+        _showDialog.value = false
+        _tasks.add(TaskModel(task = task))
+    }
+
+    fun onCheckBocSelected(taskModel: TaskModel) {
+        val index = _tasks.indexOf(taskModel)
+        _tasks[index] = _tasks[index].let {
+            it.copy(selected = !it.selected)
+        }
+    }
+
+    fun onItemRemove(taskModel: TaskModel){
+        val task = _tasks.find { it.id == taskModel.id }
+        _tasks.remove(task)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/kleecollage/todoapp/addtask/ui/TasksScreen.kt
+++ app/src/main/java/com/kleecollage/todoapp/addtask/ui/TasksScreen.kt
@@ -1,46 +1,66 @@
 package com.kleecollage.todoapp.addtask.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.kleecollage.todoapp.addtask.ui.model.TaskModel
 
 @Composable
 fun TasksScreen(taskViewModel: TaskViewModel, innerPadding: PaddingValues) {
+    val showDialog: Boolean by taskViewModel.showDialog.observeAsState(false)
+
     Box(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
-        AddTaskDialog(true, onDismiss = {}, onTaskAdded = {})
-        FabDialog(Modifier.align(Alignment.BottomEnd).padding(16.dp))
+        AddTaskDialog(
+            showDialog,
+            onDismiss = {taskViewModel.onShowDialog(false) },
+            onTaskAdded = {taskViewModel.onTasksCreated(it) }
+        )
+        FabDialog(Modifier.align(Alignment.BottomEnd).padding(16.dp), taskViewModel)
+        TaskList(taskViewModel)
     }
 }
 
 @Composable
-fun  FabDialog(modifier: Modifier) {
+fun  FabDialog(modifier: Modifier, taskViewModel: TaskViewModel) {
     FloatingActionButton(
-        onClick = { /* TODO Mostrar Dialogo */ },
+        onClick = { taskViewModel.onShowDialog(true) },
         modifier = modifier
     ) {
         Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
@@ -71,7 +91,7 @@ fun AddTaskDialog(show: Boolean, onDismiss:() -> Unit, onTaskAdded:(String) -> U
 
                 Spacer(modifier = Modifier.size(16.dp))
                 Button(
-                    onClick = { onTaskAdded(myTask)  },
+                    onClick = { onTaskAdded(myTask); myTask=""  },
                     modifier = Modifier.fillMaxWidth())
                 {
                     Text(text = "Añadir tarea")
@@ -81,9 +101,41 @@ fun AddTaskDialog(show: Boolean, onDismiss:() -> Unit, onTaskAdded:(String) -> U
     }
 }
 
+@Composable
+fun TaskList(taskViewModel: TaskViewModel) {
+    val myTasks:List<TaskModel> = taskViewModel.task
+    LazyColumn {
+       items(myTasks, key = {it.id} ) { task ->
+           ItemTask(task, taskViewModel)
+       }
+    }
+}
 
-
-
+@Composable
+fun ItemTask(taskModel: TaskModel, taskViewModel: TaskViewModel){
+    Card( Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .pointerInput(Unit){
+                detectTapGestures(onLongPress = {
+                    taskViewModel.onItemRemove(taskModel)
+                })
+            },
+        // border = BorderStroke(2.dp, Color.DarkGray),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task,
+                modifier = Modifier.weight(1f).padding(horizontal = 4.dp)
+            )
+            Checkbox(
+                checked = taskModel.selected,
+                onCheckedChange = {taskViewModel.onCheckBocSelected(taskModel)}
+            )
+        }
+    }
+}
 
 
 
